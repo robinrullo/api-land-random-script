7f6956bf78b13326497b725b6fe52109d06522e9
robin.rullo <robin.rullo@gmail.com>
2022-11-20 14:39:10 +0100

    fix: cors issues

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,59 @@
 import { handleRequest } from './routes/index.router'
 
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET,HEAD,POST,PUT,OPTIONS',
+  'Access-Control-Max-Age': '86400',
+}
+
+function handleOptions(request) {
+  const headers = request.headers
+  if (
+    headers.get('Origin') !== null &&
+    headers.get('Access-Control-Request-Method') !== null &&
+    headers.get('Access-Control-Request-Headers') !== null
+  ) {
+    const respHeaders = {
+      ...corsHeaders,
+      'Access-Control-Allow-Headers': request.headers.get(
+        'Access-Control-Request-Headers',
+      ),
+    }
+
+    return new Response(null, {
+      headers: respHeaders,
+    })
+  } else {
+    return new Response(null, {
+      headers: {
+        Allow: 'GET, HEAD, POST, OPTIONS',
+      },
+    })
+  }
+}
+
 addEventListener('fetch', (event) => {
-  event.respondWith(
-    handleRequest(event.request).then((res: Response) => {
-      res.headers.set('Access-Control-Allow-Origin', '*')
-      return res
-    }),
-  )
+  const request = event.request
+  const url = new URL(request.url)
+  if (request.method === 'OPTIONS') {
+    // Handle CORS preflight requests
+    event.respondWith(handleOptions(request))
+  } else if (['GET', 'HEAD', 'POST', 'PUT'].includes(request.method)) {
+    // Handle requests to the API server
+    event.respondWith(
+      handleRequest(request).then((response) => {
+        Object.entries(corsHeaders).forEach(([key, value]) => {
+          response.headers.set(key, value)
+        })
+        return response
+      }),
+    )
+  } else {
+    event.respondWith(
+      new Response(null, {
+        status: 405,
+        statusText: 'Method Not Allowed',
+      }),
+    )
+  }
 })
